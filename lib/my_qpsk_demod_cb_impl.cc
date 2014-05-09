e6ba97c24ec4e808479944d20a10f283f94efe1b
Tom Rondeau <tom@trondeau.com>
2014-05-09 18:24:28 -0400

    misc: some general fixups.

--- lib/my_qpsk_demod_cb_impl.cc
+++ lib/my_qpsk_demod_cb_impl.cc
@@ -1,17 +1,17 @@
 /* -*- c++ -*- */
-/* 
+/*
  * Copyright 2014 <+YOU OR YOUR COMPANY+>.
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -31,10 +31,10 @@
 namespace gr {
   namespace tutorial {
 
-    #define SQRT_TWO 0.707107    
-    #define INFTY 1000	
+    #define SQRT_TWO 0.707107
+    #define INFTY 1000
 //    #define DEBUG
-   		
+
     my_qpsk_demod_cb::sptr
     my_qpsk_demod_cb::make(bool differential_coding, bool Gray_code)
     {
@@ -47,10 +47,10 @@ namespace gr {
      */
     my_qpsk_demod_cb_impl::my_qpsk_demod_cb_impl(bool differential_coding, bool Gray_code)
       : gr::block("my_qpsk_demod_cb",
-              gr::io_signature::make(1, 1, sizeof(gr_complex)),
-              gr::io_signature::make(1, 1, sizeof(unsigned char))),
-	      d_differential_coding(differential_coding),
-	      d_Gray_code(Gray_code)	
+                  gr::io_signature::make(1, 1, sizeof(gr_complex)),
+                  gr::io_signature::make(1, 1, sizeof(unsigned char))),
+        d_differential_coding(differential_coding),
+        d_Gray_code(Gray_code)
     {}
 
     /*
@@ -80,12 +80,12 @@ namespace gr {
         const gr_complex *in = (const gr_complex *) input_items[0];
         unsigned char *out = (unsigned char *) output_items[0];
 	gr_complex origin = gr_complex(0,0);
-        // Perform ML decoding over the input iq data to generate alphabets 
-        for(int i = 0; i < noutput_items; i++) 
-	{	
-		// ML decoder, determine the minimum distance from all constellation points 
+        // Perform ML decoding over the input iq data to generate alphabets
+        for(int i = 0; i < noutput_items; i++)
+	{
+		// ML decoder, determine the minimum distance from all constellation points
 		out[i] = get_minimum_distances(&(in[i]));
-      	}	
+      	}
 
 	// Tell runtime system how many input items we consumed on
         // each input stream.
@@ -96,47 +96,47 @@ namespace gr {
     }
 
     unsigned char
-    my_qpsk_demod_cb_impl::get_minimum_distances(const gr_complex *sample)	
+    my_qpsk_demod_cb_impl::get_minimum_distances(const gr_complex *sample)
     {
 	unsigned char index, k;
 	std::vector<float> s(3,0);
         gr_complex const_pt;
 	float min_distance = INFTY;
 	for(k = 0; k < 4; k++)
-	{   
+	{
 	    if (d_Gray_code ==  true)
 	    {
-#ifdef DEBUG		
+#ifdef DEBUG
 		printf("Gray Code is enabled\n");
 #endif
 	   	switch(k)
 	    	{
-		    case 0:
+                    case 0:
 		    {
-		    	// (0,1)	
+		    	// (0,1)
 			const_pt = gr_complex(SQRT_TWO, -SQRT_TWO);
-		        break; 
-	            }		
+		        break;
+	            }
 		    case 1:
 	  	    {
-		        // (1,1)	
+		        // (1,1)
 		   	const_pt = gr_complex(-SQRT_TWO, -SQRT_TWO);
-		   	break; 
-		    }	
+		   	break;
+		    }
 	   	    case 2:
 		    {
-		        // (1,0)	
+		        // (1,0)
 		    	const_pt = gr_complex(-SQRT_TWO, SQRT_TWO);
-		  	break; 
-		    }	
+		  	break;
+		    }
 		    case 3:
 	  	    {
-		    	// (0,0)	
+		    	// (0,0)
 		   	const_pt = gr_complex(SQRT_TWO, SQRT_TWO);
-		   	break; 
+		   	break;
 		    }
 		    default:
-			printf("Invalid option\n");	
+			printf("Invalid option\n");
 	        }
 	    }
 	    else
@@ -148,53 +148,52 @@ namespace gr {
 	    	{
 		    case 0:
 		    {
-		    	// (0,1)	
+		    	// (0,1)
 			const_pt = gr_complex(SQRT_TWO, -SQRT_TWO);
-		        break; 
-	            }		
+		        break;
+	            }
 		    case 1:
 	  	    {
-		        // (1,1)	
+		        // (1,1)
 		   	const_pt = gr_complex(-SQRT_TWO, -SQRT_TWO);
-		   	break; 
-		    }	
+		   	break;
+		    }
 	   	    case 2:
 		    {
-		        // (1,0)	
+		        // (1,0)
 		    	const_pt = gr_complex(-SQRT_TWO, SQRT_TWO);
-		  	break; 
-		    }	
+		  	break;
+		    }
 		    case 3:
 	  	    {
-		    	// (0,0)	
+		    	// (0,0)
 		   	const_pt = gr_complex(SQRT_TWO, SQRT_TWO);
-		   	break; 
+		   	break;
 		    }
 		    default:
-			printf("Invalid option\n");	
+			printf("Invalid option\n");
 	        }
 	    }
 	    s[k] = get_euclidean_distance(&const_pt, sample);
          }
-	
+
          // Mapping back the symbols to bytes
 	 for (k = 0; k < 4 ; k++)
 	 {
-	     if (s[k] < min_distance)   
+	     if (s[k] < min_distance)
 	     {
 	         min_distance = s[k];
 		 index = k;
-		 printf("alphabet = %u \n",index);
- 	     }	
+		 //printf("alphabet = %u \n",index);
+ 	     }
 	 }
 	 return index;
-    }	        
-	
+    }
+
     float
     my_qpsk_demod_cb_impl::get_euclidean_distance(gr_complex *d1, const gr_complex *d2)
     {
-    	return ((d1->real() - d2->real()) * (d1->real() - d2->real()) +  ((d1->imag() - d2->imag()) * (d1->imag() - d2->imag()))); 
-    }		 	
+    	return ((d1->real() - d2->real()) * (d1->real() - d2->real()) +  ((d1->imag() - d2->imag()) * (d1->imag() - d2->imag())));
+    }
   } /* namespace tutorialtutorial  */
 } /* namespace gr */
-
--- swig/tutorial_swig.i
+++ swig/tutorial_swig.i
@@ -8,6 +8,9 @@
 %include "tutorial_swig_doc.i"
 
 %{
+#include "tutorial/my_qpsk_demod_cb.h"
 %}
 
+%include "tutorial/my_qpsk_demod_cb.h"
 
+GR_SWIG_BLOCK_MAGIC2(tutorial, my_qpsk_demod_cb);
