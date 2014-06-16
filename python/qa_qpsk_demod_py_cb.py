d856bea0945c3dd214bfda81d56e47088a0c3625
muniza <muniza@seas.upenn.edu>
2014-06-16 00:24:39 -0700

    added python blocks

---
+++ python/qa_qpsk_demod_py_cb.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2014 Free Software Foundation, Inc.
+# 
+# This file is part of GNU Radio
+# 
+# GNU Radio is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+# 
+# GNU Radio is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with GNU Radio; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+# 
+
+from gnuradio import gr, gr_unittest
+from gnuradio import blocks
+from qpsk_demod_py_cb import qpsk_demod_py_cb
+
+class qa_qpsk_demod_py_cb (gr_unittest.TestCase):
+
+    def setUp (self):
+        self.tb = gr.top_block ()
+
+    def tearDown (self):
+        self.tb = None
+
+    def test_001_t (self):
+        gray_code = False
+        src_data = ((-1-1j), (-1+1j), (1+1j), (1-1j))
+        expected_result = (0, 1, 2, 3)
+        src = blocks.vector_source_c (src_data)
+        fxn = qpsk_demod_py_cb (gray_code)
+        dst = blocks.vector_sink_b ()
+        self.tb.connect (src, fxn)
+        self.tb.connect (fxn, dst)
+        self.tb.run ()
+        result_data = dst.data ()
+        self.assertFloatTuplesAlmostEqual (expected_result, result_data, 6)
+
+    def test_002_t (self):
+        gray_code = True
+        src_data = ((-1-1j), (-1+1j), (1+1j), (1-1j))
+        expected_result = (0, 1, 3, 2)
+        src = blocks.vector_source_c (src_data)
+        fxn = qpsk_demod_py_cb (gray_code)
+        dst = blocks.vector_sink_b ()
+        self.tb.connect (src, fxn)
+        self.tb.connect (fxn, dst)
+        self.tb.run ()
+        result_data = dst.data ()
+        self.assertFloatTuplesAlmostEqual (expected_result, result_data, 6)
+
+if __name__ == '__main__':
+    gr_unittest.run(qa_qpsk_demod_py_cb, "qa_qpsk_demod_py_cb.xml")
---
+++ python/qpsk_demod_py_cb.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2014 Free Software Foundation, Inc.
+# 
+# This file is part of GNU Radio
+# 
+# GNU Radio is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+# 
+# GNU Radio is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with GNU Radio; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+# 
+
+import numpy
+from gnuradio import gr
+
+class qpsk_demod_py_cb(gr.sync_block):
+    """
+    docstring for block qpsk_demod_py_cb
+    """
+    def __init__(self, gray_code):
+        self.gray_code = gray_code
+        gr.sync_block.__init__(self,
+            name="qpsk_demod_py_cb",
+            in_sig=[numpy.complex64],
+            out_sig=[numpy.uint8])
+
+    def get_minimum_distances(self, sample):
+        if self.gray_code == 1:
+            if (sample.imag >= 0 and sample.real >= 0):
+                return 3 # 1+1j
+            elif (sample.imag >= 0 and sample.real < 0):
+                return 1 # -1+1j
+            elif (sample.imag < 0 and sample.real < 0):
+                return 0 # -1-1j
+            elif (sample.imag < 0 and sample.real >= 0):
+                return 2 # 1-1j
+        else:
+            if (sample.imag >= 0 and sample.real >= 0):
+                return 2 # 1+1j
+            elif (sample.imag >= 0 and sample.real < 0):
+                return 1 # -1+1j
+            elif (sample.imag < 0 and sample.real < 0):
+                return 0 # -1-1j
+            elif (sample.imag < 0 and sample.real >= 0):
+                return 3 # 1-1j
+
+    def work(self, input_items, output_items):
+        in0 = input_items[0]
+        out = output_items[0]
+        
+        for i in range(0, len(in0)):
+            sample = in0[i]
+            out[i] = self.get_minimum_distances(sample)
+           
+        return len(output_items[0])
+
